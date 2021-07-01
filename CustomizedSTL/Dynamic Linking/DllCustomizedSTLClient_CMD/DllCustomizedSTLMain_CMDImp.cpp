28a2c02ca8febf51a5b59346f2dd523536669bcd
PhoeniX-d <prnv24choudhary@gmail.com>
2021-07-01 22:47:58 +0530

    DLL CSTL

---
+++ CustomizedSTL/Dynamic Linking/DllCustomizedSTLClient_CMD/DllCustomizedSTLMain_CMDImp.cpp
@@ -0,0 +1,240 @@
+#include "DllCustomizedSTL_CMD.h"
+
+//Entry Point Function
+int main()
+{
+
+	//Objects of GEN_SLLL Class
+	//You can Replace GEN_SLLL with other classes linke GEN_SCLL,GEN_DLLL,GEN_DCLL
+	GEN_SLLL<int> iobj1, iobj2;
+	GEN_SLLL<char> cobj1;
+	GEN_SLLL<float> fobj1;
+	GEN_SLLL<double> dobj1;
+
+	//Objects of GEN_QUEUE Class
+	GEN_QUEUE<int> qiobj;
+	GEN_QUEUE<char> qcobj;
+	GEN_QUEUE<float> qfobj;
+	GEN_QUEUE<double> qdobj;
+
+	//Objects of GEN_STACK Class
+	GEN_STACK<float> siobj(10);
+	GEN_STACK<char> scobj(5);
+
+	//Objects of GEN_PRI_QUEUE Class
+	GEN_PRI_QUEUE<int> pqiobj;
+
+	//Objects of GEN_TREE class
+	GEN_TREE<char> tobj;
+
+	//Local Variables
+	int ch = 0;
+	int iRet = 0;
+	char cRet = '\0';
+
+	//Menu driven
+	cout << "1. Linked List\n";
+	cout << "2. Queue\n";
+	cout << "3. Stack\n";
+	cout << "4. Priority Queue\n";
+	cout << "5. Binary Search Tree\n";
+	cout << "Enter choice\n";
+	cin >> ch;
+
+	switch (ch)
+	{
+	case 1:
+		cout << "InsertFirst in first linked list\n\n";
+		iobj1.InsertFirst(40);
+		iobj1.InsertFirst(30);
+		iobj1.InsertFirst(20);
+		iobj1.InsertFirst(10);
+		iobj1.Display();
+		cout << "Number of Nodes in first linked list:\t" << iobj1.Count() << "\n\n";
+
+		cout << "InsertFirst in second linked list\n\n";
+		iobj2.InsertFirst(400);
+		iobj2.InsertFirst(300);
+		iobj2.InsertFirst(200);
+		iobj2.InsertFirst(100);
+		iobj2.Display();
+		cout << "Number of Nodes in second linked list:\t" << iobj1.Count() << "\n\n";
+
+		cout << "Conct two linked list and store resultant linked list in first linked list\n";
+		iobj1.Concat(iobj1, iobj2);
+		iobj1.Display();
+		cout << "Number of Nodes in first linked list:\t" << iobj1.Count() << "\n\n";
+
+		cout << "InsertLast\n\n";
+		iobj1.InsertLast(1111);
+		iobj1.InsertLast(505);
+		iobj1.InsertLast(105);
+		iobj1.InsertLast(155);
+		iobj1.Display();
+		cout << "Number of Nodes in first linked list:\t" << iobj1.Count() << "\n\n";
+
+		cout << "InsertAtPos\n\n";
+		iobj1.InsertAtPos(1000, 11);
+		iobj1.Display();
+		cout << "Number of Nodes in first linked list:\t" << iobj1.Count() << "\n\n";
+
+		cout << "DeleteFirst\n\n";
+		iobj1.DeleteFirst();
+		iobj1.Display();
+		cout << "Number of Nodes in first linked list:\t" << iobj1.Count() << "\n\n";
+
+		cout << "DeleteLast\n\n";
+		iobj1.DeleteLast();
+		iobj1.Display();
+		cout << "Number of Nodes in first linked list:\t" << iobj1.Count() << "\n\n";
+
+		cout << "DeleteAtPos\n\n";
+		iobj1.DeleteAtPos(3);
+		iobj1.Display();
+
+		cout << "ReverseList\n\n";
+		iobj1.ReverseList();
+		iobj1.Display();
+		cout << "Number of Nodes in first linked list:\t" << iobj1.Count() << "\n\n";
+
+		cobj1.InsertFirst('P');
+		cobj1.InsertFirst('Q');
+		cobj1.InsertFirst('R');
+
+		cobj1.Display();
+
+		cout << iobj1.Frequency(10) << endl;
+		cout << iobj1.FirstOccurance(10) << endl;
+		cout << iobj1.LastOccurance(10) << endl;
+		cout << iobj1.Largest() << endl;
+		cout << iobj1.Smallest() << endl;
+
+		iobj1.Display();
+		cout << "\n\n";
+		iobj2.Display();
+		
+		break;
+
+	case 2:
+		qcobj.Enqueue('P');
+		qcobj.Enqueue('R');
+
+		qcobj.Display();
+		cout << "\n"
+			 << qcobj.Count() << "\n";
+
+		cout << qcobj.Dequeue() << "\t";
+		cout << qcobj.Dequeue() << "\t";
+		iRet = qcobj.Dequeue();
+		cout << iRet << "\n";
+
+		qdobj.Enqueue(11.21);
+		qdobj.Enqueue(21.51);
+
+		qdobj.Display();
+		cout << "\n"
+			 << qdobj.Count() << "\n";
+
+		cout << qdobj.Dequeue() << "\t";
+		cout << qdobj.Dequeue() << "\t";
+		cout << qdobj.Dequeue() << "\t";
+		break;
+
+	case 3:
+		cout << "\nElement Pushed\n";
+		siobj.Push(10.25);
+		siobj.Push(20.36);
+		siobj.Push(30.18);
+		siobj.Push(40.69);
+		siobj.Display();
+
+		cout << "\nElement Poped\n";
+		cout << siobj.Pop() << "\t";
+		cout << siobj.Pop() << "\t";
+		cout << siobj.Pop() << "\t";
+		cout << siobj.Pop() << "\t";
+		cout << siobj.Pop() << "\t";
+
+		cout << "\nElement Pushed\n";
+		scobj.Push('P');
+		scobj.Push('Q');
+		scobj.Push('R');
+		scobj.Display();
+
+		cout << "\nElement Poped\n";
+		cout << scobj.Pop() << "\t";
+		cout << scobj.Pop() << "\t";
+		cout << scobj.Pop() << "\t";
+		cout << scobj.Pop() << "\t";
+		break;
+
+	case 4:
+		pqiobj.Enqueue(11, 25);
+		pqiobj.Enqueue(16, 27);
+		pqiobj.Enqueue(19, 18);
+		pqiobj.Enqueue(40, 29);
+		pqiobj.Enqueue(11, 1);
+		pqiobj.Enqueue(17, 6);
+		pqiobj.Enqueue(25, 16);
+		pqiobj.Enqueue(36, 13);
+		pqiobj.Enqueue(51, 35);
+		pqiobj.Enqueue(71, 5);
+		pqiobj.Enqueue(91, 15);
+		pqiobj.Enqueue(81, 20);
+
+		pqiobj.Display();
+		cout << pqiobj.Count() << endl;
+
+		cout << pqiobj.Dequeue() << endl;
+		cout << pqiobj.Dequeue() << endl;
+		cout << pqiobj.Dequeue() << endl;
+		cout << pqiobj.Dequeue() << endl;
+		cout << pqiobj.Dequeue() << endl;
+		cout << pqiobj.Dequeue() << endl;
+		cout << pqiobj.Dequeue() << endl;
+		cout << pqiobj.Dequeue() << endl;
+		cout << pqiobj.Dequeue() << endl;
+		cout << pqiobj.Dequeue() << endl;
+		cout << pqiobj.Dequeue() << endl;
+
+	case 5:
+		tobj.Insert('H');
+		tobj.Insert('E');
+		tobj.Insert('L');
+		tobj.Insert('O');
+		tobj.Insert('W');
+		tobj.Insert('R');
+
+		printf("Inorder\n");
+		tobj.Inorder();
+
+		printf("\nPreorder\n");
+		tobj.Preorder();
+
+		printf("\nPostorder\n");
+		tobj.Postorder();
+
+		iRet = tobj.Count();
+		printf("\n%d Number of Nodes \n", iRet);
+
+		iRet = tobj.CountLeaf();
+		printf("%d Nummber of Leaf Nodes  \n", iRet);
+
+		iRet = tobj.CountNonLeaf();
+		printf("%d Nummber of Non Leaf Nodes  \n", iRet);
+
+		cRet = tobj.Maximum();
+		printf("%c is Maximum Node  \n", cRet);
+
+		cRet = tobj.Minimum();
+		printf("%c Minimum Node is \n", cRet);
+
+		//cout<<"Enter the node to be Searched\n";
+		iRet = tobj.Level('H');
+		printf("Level of Node H Is %d  \n", iRet);
+		tobj.DeleteGEN_TREE();
+	}
+	return 0;
+}
+
+/////////////////////////////////////////////// End of CSTLMain.cpp ///////////////////////////////////////
---
+++ CustomizedSTL/Dynamic Linking/DllCustomizedSTLClient_IDE/include/DllCustomizedSTL_IDE.h
@@ -0,0 +1,356 @@
+#pragma once
+#ifdef EXP_CLASS
+#define DECLSPEC __declspec(dllexport)
+#else
+#define DECLSPEC __declspec(dllimport)
+#endif
+
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+#define TRUE 1
+#define FALSE 0
+
+// Generalized Structure of node For Singly Linked List
+template <class T>
+struct node
+{
+	T Data;
+	struct node* Next;
+};
+
+// Generalized Structure of node For Queue
+template <class T>
+struct qnode
+{
+	int P;
+	T Data;
+	struct qnode* Next;
+};
+
+// Generalized Structure of node For GEN_STACK
+template <class T>
+struct snode
+{
+	T Data;
+	struct snode* Next;
+};
+
+// Generalized Structure of node For Doubly Linked List
+template <class T>
+struct dnode
+{
+	T Data;
+	struct dnode* Next;
+	struct dnode* Prev;
+};
+
+template <class T>
+struct tnode
+{
+	T Data;
+	struct tnode<T>* lchild;
+	struct tnode<T>* rchild;
+};
+
+//Abstract Class of Linked_List
+template <class T>
+class Linked_List
+{
+protected:
+	virtual void InsertFirst(T) = 0;
+	virtual void InsertLast(T) = 0;
+	virtual void InsertAtPos(T, int) = 0;
+	virtual void Display() = 0;
+	virtual int Count() = 0;
+	virtual void DeleteFirst() = 0;
+	virtual void DeleteLast() = 0;
+	virtual void DeleteAtPos(int) = 0;
+};
+
+///////////////////////////////////////////////////////////////////////
+//
+//	ClassName 		:GEN_SLLL
+//	Description		:This is class designed for Singly linear linked list
+//	Author			:Pranav Choudhary
+///////////////////////////////////////////////////////////////////////
+
+//Generic Class For Singly Linear Linked List
+
+template <class T>
+class DECLSPEC GEN_SLLL : protected Linked_List<T>
+{
+private:
+	struct node<T>* Head; //Start Pointer
+	struct node<T>* Tail; //End  Pointer
+	int size;
+
+public:
+	GEN_SLLL();
+	~GEN_SLLL();
+	void InsertFirst(T);
+	void InsertLast(T);
+	void InsertAtPos(T, int);
+	void Display();
+	void DisplayR();
+	int Count();
+	void DeleteFirst();
+	void DeleteLast();
+	void DeleteAtPos(int);
+	int Frequency(T);
+	int FirstOccurance(T);
+	int LastOccurance(T);
+	T Largest();
+	T Smallest();
+	void ReverseList();
+	void Concat(GEN_SLLL<T>& op1, GEN_SLLL<T>& op2);
+};
+
+///////////////////////////////////////////////////////////////////////
+//
+//	ClassName 		:GEN_SCLL
+//	Description		:This is class designed Singly Circular linked list
+//	Author			:Pranav Choudhary
+//
+///////////////////////////////////////////////////////////////////////
+
+template <class T>
+class DECLSPEC GEN_SCLL : protected Linked_List<T>
+{
+private:
+	struct node<T>* Head;
+	struct node<T>* Tail;
+	T size;
+
+public:
+	GEN_SCLL();
+	~GEN_SCLL();
+
+	void InsertFirst(T);
+	void InsertLast(T);
+	void InsertAtPos(T, int);
+	void DeleteFirst();
+	void DeleteLast();
+	void DeleteAtPos(int);
+	void Display();
+	int Frequency(T);
+	int FirstOccurance(T);
+	int LastOccurance(T);
+	T Largest();
+	T Smallest();
+	int Count();
+	void Concat(GEN_SCLL<T>& op1, GEN_SCLL<T>& op2);
+};
+
+///////////////////////////////////////////////////////////////////////
+//
+//	ClassName 		:GEN_DLLL
+//	Description		:This is class designed for Doubly linear linked list
+//	Author			:Pranav Choudhary
+///////////////////////////////////////////////////////////////////////
+
+//Generic Class For Singly Circular Linked List
+
+template <class T>
+class DECLSPEC GEN_DLLL : protected Linked_List<T>
+{
+private:
+	struct dnode<T>* Head;
+	struct dnode<T>* Tail;
+	int size;
+
+public:
+	GEN_DLLL();
+	~GEN_DLLL();
+
+	void InsertFirst(T);
+	void InsertLast(T);
+	void InsertAtPos(T, int);
+	void DeleteFirst();
+	void DeleteLast();
+	void DeleteAtPos(int);
+	void Display();
+	void DisplayB();
+	int Count();
+	int Frequency(T);
+	int FirstOccurance(T);
+	int LastOccurance(T);
+	T Largest();
+	T Smallest();
+	void Concat(GEN_DLLL<T>& op1, GEN_DLLL<T>& op2);
+};
+
+///////////////////////////////////////////////////////////////////////
+//
+//	ClassName 		:GEN_DCLL
+//	Description		:This is class designed for Doubly Circular linked list
+//	Author			:Pranav Choudhary
+///////////////////////////////////////////////////////////////////////
+
+//Generic Class For Doubly Linear Linked List
+
+template <class T>
+class DECLSPEC GEN_DCLL : protected Linked_List<T>
+{
+private:
+	struct dnode<T>* Head;
+	struct dnode<T>* Tail;
+	int size;
+
+public:
+	GEN_DCLL();
+	~GEN_DCLL();
+
+	void InsertFirst(T);
+	void InsertLast(T);
+	void InsertAtPos(T, int);
+	void DeleteFirst();
+	void DeleteLast();
+	void DeleteAtPos(int);
+	void Display();
+	void DisplayR();
+	int Count();
+	int Frequency(T);
+	int FirstOccurance(T);
+	int LastOccurance(T);
+	T Largest();
+	T Smallest();
+	void Concat(GEN_DCLL<T>& op1, GEN_DCLL<T>& op2);
+};
+
+///////////////////////////////////////////////////////////////////////
+//
+//	ClassName 		:GEN_QUEUE
+//	Description		:This is class designed for Queue
+//	Author			:Pranav Choudhary
+///////////////////////////////////////////////////////////////////////
+
+//Generic Class For Queue
+
+template <class T>
+struct Node
+{
+	T Data;
+	struct Node* Next;
+};
+
+template <class T>
+class DECLSPEC GEN_QUEUE
+{
+private:
+	struct Node<T>* Head;
+	struct Node<T>* Tail;
+
+public:
+	GEN_QUEUE();
+	~GEN_QUEUE();
+	void Enqueue(T);
+	T Dequeue();
+	void Display();
+	int Count();
+};
+
+//////////////////////////////////////////////////////////////
+//
+//	ClassName 		:GEN_PRI_QUEUE
+//	Description		:This is class designed for priority queue
+//	Author			:Pranav Choudhary
+//
+//////////////////////////////////////////////////////////////
+
+//Generic Class For Priority Queue
+
+template <class T>
+class DECLSPEC GEN_PRI_QUEUE
+{
+private:
+	struct qnode<T>* Front;
+	struct qnode<T>* Rear;
+	int size;
+
+public:
+	GEN_PRI_QUEUE();
+	~GEN_PRI_QUEUE();
+	void Enqueue(T, int);
+	T Dequeue();
+	void Display();
+	int Count();
+};
+
+///////////////////////////////////////////////////////////////////
+//	
+//	ClassName 		:GEN_TREE
+//	Description		:This is class designed for Binary Search Tree
+//	Author			:Pranav Choudhary
+//
+///////////////////////////////////////////////////////////////////
+
+template <class T>
+class DECLSPEC GEN_TREE
+{
+private:
+	struct tnode<T>* Head;
+	void Insert(struct tnode<T>**, T);
+	void Inorder(struct tnode<T>*);
+	void Preorder(struct tnode<T>*);
+	void Postorder(struct tnode<T>*);
+	int Count(struct tnode<T>*);
+	bool Search(struct tnode<T>*, T);
+	void DeleteGEN_TREE(struct tnode<T>*);
+	int CountLeaf(struct tnode<T>*);
+	int CountNonLeaf(struct tnode<T>*);
+	int Level(struct tnode<T>*, T);
+	T Minimum(struct tnode<T>*);
+	T Maximum(struct tnode<T>*);
+
+public:
+	GEN_TREE();
+	void Insert(T);
+	void Inorder();
+	void Preorder();
+	void Postorder();
+	int Count();
+	bool Search(T);
+	void DeleteGEN_TREE();
+	int CountLeaf();
+	int CountNonLeaf();
+	int Level(T);
+	T Minimum();
+	T Maximum();
+};
+
+///////////////////////////////////////////////////////////////////
+//	
+//	ClassName 		:GEN_TREE
+//	Description		:This is class designed for stack
+//	Author			:Pranav Choudhary
+//
+///////////////////////////////////////////////////////////////////
+template<class T>
+class DECLSPEC GEN_STACK
+{
+private:
+	T* Arr;
+	int iTop;
+	int iSize;
+
+public:
+	/* Parameterized constructor with default args */
+	GEN_STACK(int);
+	/* Destructor */
+	~GEN_STACK();
+	/* IsEmpty: check stack is empty or not */
+	bool IsEmpty();
+	/* IsFull: check stack is full or not */
+	bool IsFull();
+	/* Push: Push element in stack */
+	void Push(T);
+	/* Pop: Pop element from stack */
+	T Pop();
+	/* Display elements in stack */
+	void Display();
+
+};// end of class GEN_STACK declaration
+
+////////////////////////////////////////////// End of CSTL.h ///////////////////////////////////////////
