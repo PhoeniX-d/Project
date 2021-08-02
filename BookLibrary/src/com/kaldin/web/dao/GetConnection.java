61ef5897081ebd9d653bf1a7017213c7c8b53de0
PhoeniX-d <prnv24choudhary@gmail.com>
2021-08-02 09:30:23 +0530

    Updated with resources file

--- BookLibrary/src/com/kaldin/web/dao/GetConnection.java
+++ BookLibrary/src/com/kaldin/web/dao/GetConnection.java
@@ -1,21 +1,33 @@
 package com.kaldin.web.dao;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class GetConnection {
 
-	private String jdbcURL = "jdbc:mysql://localhost:3306/booklib";
-	private String jdbcUsername = "root";
-	private String jdbcPassword = "";
+	private String jdbcURL;
+	private String jdbcUsername;
+	private String jdbcPassword;
 	private Connection con = null;
 
 	// Get connection reference
 	protected Connection getConnection() {
+		Properties p = new Properties();
+		InputStream is;
 		try {
+			is = this.getClass().getResourceAsStream("/resources/db.properties");
+			p.load(is);
+			jdbcURL = p.getProperty("MysqlURL");
+			jdbcUsername = p.getProperty("MysqlUser");
+			jdbcPassword = p.getProperty("MysqlPwd");
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+		} catch (IOException io) {
+			io.printStackTrace();
 		} catch (SQLException sqle) {
 			printSQLException(sqle);
 		} catch (ClassNotFoundException cnf) {
